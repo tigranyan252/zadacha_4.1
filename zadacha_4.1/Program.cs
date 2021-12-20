f1d6316cc3073bd498091f09f7836ede72a15942
Serj Tigranyan <serj-tigranyan@mail.ru>
2021-12-20 13:10:49 +0200

    Добавьте файлы проекта.

---
+++ zadacha_4.1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace zadacha_4._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number");
+            char[] num = Console.ReadLine().ToCharArray();
+            char t = num[num.Length - 1];
+            for .(int i = num.Length - 1; i > 0; i--)
+            {
+                num[i] = num[i - 1];
+            }
+            num[0] = t;
+            for (int i = 0; i < num.Length; i++)
+            {
+                Console.WriteLine(num[i]);
+            }
+        }
+    }
+}
